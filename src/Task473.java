22ac110c1dc0268a2d83b358bb444df487d549c8
Sergej <luzhins764@gmail.com>
2024-10-10 10:32:27 +0300

    Added Scanner

--- src/Task473.java
+++ src/Task473.java
@@ -28,7 +28,8 @@ public class Task473 {
 
     public static void main(String[] args) {
         System.out.println("Enter your number: ");
-        String number = "99999999";
+        Scanner scanner = new Scanner(System.in);
+        String number = scanner.next();
         printDecimal(number);
     }
 }
