264c3d87386aad3ce379f8db41e868eb4591cccc
Sergej <luzhins764@gmail.com>
2024-10-10 10:29:32 +0300

    First Commit

---
+++ src/Task473.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Task473 {
+
+    public static String getDecimal(StringBuilder number) {
+        for (int i = number.length() - 1; i >= 0; i--) {
+            if (i % 3 == 0) {
+                char temp_for_dot = number.charAt(i + 1);
+                number.setCharAt(i + 1, ',');
+                for (int j = i + 2; j < number.length() - 1; j++) {
+                    char temp = number.charAt(j);
+                    //OVERTHOUGHT
+                }
+            }
+        }
+        String decimal = number.toString();
+        return decimal;
+    }
+
+    public static void printDecimal(String number) {
+        for (int i = 0; i < number.length(); i++) {
+            System.out.print(number.charAt(i));
+            if ((number.length() - 1 - i) % 3 == 0 && i != number.length() - 1) {
+                System.out.print(",");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Enter your number: ");
+        String number = "99999999";
+        printDecimal(number);
+    }
+}
